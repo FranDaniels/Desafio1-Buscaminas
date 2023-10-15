d55811de13c10ccfc28ffcaa2f26d071106c6567
Francisco <coheteomega@gmail.com>
2023-10-15 19:11:02 +0200

    Revert "no message"
    This reverts commit 1644f95126bbbb68118b7198c5a47670c7f57ecb.

--- Clases/factoria.php
+++ Clases/factoria.php
@@ -13,47 +13,4 @@ class factoria{
         $partida=new partida($idPartida,$idUsuario,$tableroOculto,$tableroMostrado,$finalizado);
         return $partida;
     }
-
-    public static function crearTableroRelleno($tamanio,$cantBombas){
-        $tablero=array_fill(1,$tamanio,0);
-
-        while ($cantBombas>0) {
-            $posicion=rand(1,$tamanio);
-
-            if ($tablero[$posicion]!='*') { //Recordar que para las bombas se utilizará el símbolo *
-                $tablero[$posicion]='*'; //Si la casilla no contiene una bomba la colocaremos
-
-                --$cantBombas;
-            }
-        }
-
-        for ($i=1; $i <= $tamanio; $i++) { 
-            if ($tablero[$i]!='*') {
-                if ($i>1 && $tablero[$i-1]=='*') { //Comprobación de la casilla de la izquierda
-                    $tablero[$i]++;
-                }
-                if ($i>1 && $tablero[$i+1]=='*') { //Comprobación de la casilla de la derecha
-                    $tablero[$i]++;
-                }
-            }
-        }
-
-        $tableroRelleno=implode('',$tablero);
-
-        return $tableroRelleno;
-    }
-
-    public static function crearTableroNormal($tamanio){
-        $tableroNormal=implode('',array_fill(1,$tamanio,'-'));
-
-        return $tableroNormal;
-    }
-
-    public static function creacionPartida($idPartida,$idJugador,$tamanioTablero,$cantBombas){
-        $tableroResuelto=factoria::crearTableroRelleno($tamanioTablero,$cantBombas);
-        $tableroJugador=factoria::crearTableroNormal($tamanioTablero);
-        $partida=factoria::crearPartida($idPartida,$idJugador,$tableroResuelto,$tableroJugador,false);//Preguntar sobre el idPartida a Fernando
-
-        return $partida;
-    }
 }
